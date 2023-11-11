4b2c8a9d10f9c58fad3e9273d3e478299170a434
rimrim990 <lim990718@naver.com>
2023-11-11 13:08:01 +0900

    feat: 정적 파일 브라우저 캐싱

--- src/main/java/com/gugucon/shopping/config/WebConfig.java
+++ src/main/java/com/gugucon/shopping/config/WebConfig.java
@@ -2,8 +2,11 @@ package com.gugucon.shopping.config;
 
 import com.gugucon.shopping.member.controlller.converter.StringToBirthYearRangeConverter;
 import com.gugucon.shopping.member.controlller.converter.StringToGenderConverter;
+import java.util.concurrent.TimeUnit;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -12,5 +15,12 @@ public class WebConfig implements WebMvcConfigurer {
     public void addFormatters(FormatterRegistry registry) {
         registry.addConverter(new StringToBirthYearRangeConverter());
         registry.addConverter(new StringToGenderConverter());
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/assets/**", "/js/**", "/css/**")
+            .addResourceLocations("classpath:/static/assets/", "classpath:/static/css/", "classpath:/static/js/")
+            .setCacheControl(CacheControl.maxAge(1, TimeUnit.DAYS));
     }
 }
